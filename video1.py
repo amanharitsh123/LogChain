2611400ba125204c773c5e90702fb225285ce63e
amanharitsh123 <amanharitsh123@gmail.com>
2019-10-12 13:00:53 +0530

    video2

--- video1.py
+++ video1.py
@@ -5,5 +5,6 @@ web3 = Web3(Web3.HTTPProvider(infura_url))
 
 print(web3.isConnected())
 
-balance = web3.eth.getBalance("")
-
+balance = web3.eth.getBalance("0xA469FbEED8499A929c0E63aeb5F58951CF616D9b")
+print(balance)
+print(web3.fromWei(balance,'ether'))
